c7e5c87b23074ada523078908318b33060f09733
theLN3 <ln3@users.noreply.github.com>
2023-09-23 16:11:30 +0200

    .

--- First Angular project/FirstAngularApp/src/app/app.component.ts
+++ First Angular project/FirstAngularApp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'My first Application with Angular and VS Code';
+  title = 'My first Application with Angular and VS Code - just a try in getting further in coding;
 }
